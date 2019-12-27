e01290cafd67c11cd0488db10efa9603df9560ab
Niek125 <niek.sleddens@gmail.com>
2019-12-27 18:28:35 +0100

    zipkin and logs kafka

--- src/main/java/com/niek125/eurekaserver/controller/ServiceController.java
+++ src/main/java/com/niek125/eurekaserver/controller/ServiceController.java
@@ -2,6 +2,8 @@ package com.niek125.eurekaserver.controller;
 
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.eureka.registry.PeerAwareInstanceRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/server-instances")
 public class ServiceController {
+    private final Logger logger = LoggerFactory.getLogger(ServiceController.class);
     private final PeerAwareInstanceRegistry registry;
     private int index;
 
@@ -22,17 +25,17 @@ public class ServiceController {
     }
 
     @RequestMapping("/getinstance/{service-name}")
-    public String getInstance(@PathVariable("service-name") String service){
-        try {
-            List<InstanceInfo> instances = this.registry.getApplications().getRegisteredApplications(service).getInstancesAsIsFromEureka();
-            index++;
-            if (index >= instances.size()){
-                index = 0;
-            }
-            return instances.get(index).getIPAddr() + ":" + instances.get(index).getPort();
-        }catch (Exception e) {
+    public String getInstance(@PathVariable("service-name") String service) {
+        logger.info("getting instances for: " + service);
+        List<InstanceInfo> instances = this.registry.getApplications().getRegisteredApplications(service).getInstancesAsIsFromEureka();
+        index++;
+        if (index >= instances.size()) {
+            index = 0;
         }
-        return null;
+        logger.info(String.format("picking %x out of %x", index + 1, instances.size()));
+        String ip = instances.get(index).getIPAddr() + ":" + instances.get(index).getPort();
+        logger.info("returning: " + ip);
+        return ip;
     }
 
 }
