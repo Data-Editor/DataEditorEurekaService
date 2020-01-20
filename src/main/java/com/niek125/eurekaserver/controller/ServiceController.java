6a347f255acb7f423619541a0a72bd8d2ea12e3b
Niek125 <niek.sleddens@gmail.com>
2020-01-20 05:40:23 +0100

    Fix corsconfig

--- src/main/java/com/niek125/eurekaserver/controller/ServiceController.java
+++ src/main/java/com/niek125/eurekaserver/controller/ServiceController.java
@@ -5,13 +5,12 @@ import com.netflix.eureka.registry.PeerAwareInstanceRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/server-instances")
 public class ServiceController {
     private final Logger logger = LoggerFactory.getLogger(ServiceController.class);
@@ -24,17 +23,17 @@ public class ServiceController {
         index = 0;
     }
 
-    @RequestMapping("/getinstance/{service-name}")
+    @GetMapping("/getinstance/{service-name}")
     public String getInstance(@PathVariable("service-name") String service) {
-        logger.info("getting instances for: " + service);
+        logger.info("getting instances for: {}", service);
         List<InstanceInfo> instances = this.registry.getApplications().getRegisteredApplications(service).getInstancesAsIsFromEureka();
         index++;
         if (index >= instances.size()) {
             index = 0;
         }
-        logger.info(String.format("picking %x out of %x", index + 1, instances.size()));
+        logger.info("picking {} out of {}", index + 1, instances.size());
         String ip = instances.get(index).getIPAddr() + ":" + instances.get(index).getPort();
-        logger.info("returning: " + ip);
+        logger.info("returning: {}", ip);
         return ip;
     }
 
